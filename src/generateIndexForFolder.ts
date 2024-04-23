b4e95ca5a50a8f2409642bf2bf8b487fc02c4e91
melenaos <menelaosvergis@gmail.com>
2024-04-23 19:35:49 +0300

    fix console messages for cleaner run

--- src/generateIndexForFolder.ts
+++ src/generateIndexForFolder.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 
 import { getFileList, isDirectory } from './utils';
 
-function generateIndexForFolders(srcFolder: string, maxDepth: number): boolean {
+function generateIndexForFolders(rootFolder: string, srcFolder: string, maxDepth: number): boolean {
   maxDepth--;
   if (maxDepth < 0) return false;
 
@@ -17,7 +17,7 @@ function generateIndexForFolders(srcFolder: string, maxDepth: number): boolean {
     const filename = path.basename(filePath);
 
     if (isDirectory(filePath)) {
-      var hasIndex = generateIndexForFolders(filePath, maxDepth);
+      var hasIndex = generateIndexForFolders(rootFolder, filePath, maxDepth);
       if (hasIndex) return `export * from './${filename}'`;
       else return null;
     } else if (
@@ -49,10 +49,11 @@ function generateIndexForFolders(srcFolder: string, maxDepth: number): boolean {
   try {
     fs.writeFileSync(path.join(srcFolder, 'index.ts'), fileLines.join('\n'));
   } catch (err) {
-    if (err) console.error(err);
+    console.info(chalk.red(`${srcFolder.replace(rootFolder, "")} > index.ts error: \n`));
+    console.error(err);
   }
 
-  console.info(chalk.green(`\n${srcFolder} > index.ts \n`));
+  console.info(chalk.green(`${srcFolder.replace(rootFolder, "")} > index.ts created \n`));
 
   return true;
 }
--- src/index.ts
+++ src/index.ts
@@ -9,28 +9,14 @@ import generateForFolder from './generateIndexForFolder';
 export const generateIndexForFolder = generateForFolder;
 
 export function runCli() {
-  const argv = yargs(process.argv.slice(2)).option('f', {
-    alias: 'from',
-    describe:
-      'specify if you want to use folders or files to generate the index file.',
-    type: 'string',
-    choices: ['files', 'folders'],
-  }).argv;
+  const argv = yargs(process.argv.slice(2)).argv;
+  // if we ever need some other options other than the rootFolder use yargs(process.argv.slice(2)).option('f',{...}).argv;
+
   const folder = (argv._[0] as string) || './';
   const sourceFolder = path.join(process.cwd(), folder);
 
-  if (generateIndexForFolder(sourceFolder, 10))
-    chalk.green('\n> Successfully to build the index.ts file \n');
-
-  // fs.writeFile(path.join(sourceFolder, 'index.ts'), indexContentFile, err => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-
-  //   console.log(
-  //     chalk.green('\n> Successfully to build the index.ts file :)\n')
-  //   );
-  // });
+  if (generateIndexForFolder(sourceFolder, sourceFolder, 10))
+    chalk.green('> Successfully build the index.ts files \n');
 }
 
 runCli();
